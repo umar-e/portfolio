883a9c6dc193283bbf77a64e6d8a7a7eab76ed91
Umar <m.umarbinejaz@gmail.com>
2023-09-12 19:45:10 +0500

    basic template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Unknown from './pages/Unknown';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div >
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Unknown />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <header className="mb-8 flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-16">
+          {/* logo - start */}
+          <a
+            href="/"
+            className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
+            aria-label="logo"
+          >
+            <svg
+              width={95}
+              height={94}
+              viewBox="0 0 95 94"
+              className="h-auto w-6 text-indigo-500"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M96 0V47L48 94H0V47L48 0H96Z" />
+            </svg>
+            Flowrift
+          </a>
+          {/* logo - end */}
+          {/* nav - start */}
+          <nav className="hidden gap-12 lg:flex">
+            <a href="#" className="text-lg font-semibold text-indigo-500">
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            >
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            >
+              Pricing
+            </a>
+            <a
+              href="#"
+              className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            >
+              About
+            </a>
+          </nav>
+          {/* nav - end */}
+          {/* buttons - start */}
+          <a
+            href="#"
+            className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
+          >
+            Contact Sales
+          </a>
+          <button
+            type="button"
+            className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Menu
+          </button>
+          {/* buttons - end */}
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
